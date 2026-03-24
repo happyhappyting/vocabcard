a88ed2dae661a9628185ba41d9d89b22f7b9399b
happyhappyting <sin_jing_ting@moe.edu.sg>
2026-03-24 12:07:16 +0800

    Update unit2.js

--- unit2.js
+++ unit2.js
@@ -1,5 +1,4 @@
 const unitData = [
-
     { word: "瞬间", pinyin: "shùn jiān" , meaning: "极短的时间；一刹那。", meaning_en: "in an instant" },
     { word: "粘", pinyin: "zhān", meaning: "东西互相附着或连接在一起。", meaning_en: "to glue; to paste; to stick to" },
     { word: "兴致勃勃", pinyin: "xìng zhì bó bó", meaning: "兴趣非常浓厚，情绪高昂。", meaning_en: "in high spirits; full of zest" },
@@ -7,7 +6,7 @@ const unitData = [
     { word: "蛋液", pinyin: "dàn yè", meaning: "鸡蛋打破后的液体。", meaning_en: "liquid of the egg" },
     { word: "讥讽", pinyin: "jī fěng", meaning: "用旁敲侧击的话嘲笑讽刺。", meaning_en: "to satirize; to ridicule" },
     { word: "赌气", pinyin: "dǔ qì", meaning: "因不满意或受责备而任性行动。", meaning_en: "to act in a fit of pique" },
-    { word: "嚷", pinyin: "rǎng", meaning: "大声叫喊。", meaning_en: "to shout; blurt out" }
+    { word: "嚷", pinyin: "rǎng", meaning: "大声叫喊。", meaning_en: "to shout; blurt out" }, // 这里补了逗号
     { word: "小心翼翼", pinyin: "xiǎo xīn yì yì", meaning: "非常小心，不敢疏忽。", meaning_en: "very carefully; prudent" },
     { word: "荧光笔", pinyin: "yíng guāng bǐ", meaning: "作标记用的彩色笔。", meaning_en: "highlighter" },
     { word: "抵挡", pinyin: "dǐ dǎng", meaning: "挡住；阻挡。", meaning_en: "to resist; to ward off" },
