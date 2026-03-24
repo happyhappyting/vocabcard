7c7b1f4c7c4602cb6cc33792cae28c5d52438465
happyhappyting <sin_jing_ting@moe.edu.sg>
2026-03-24 11:58:56 +0800

    Create unit1.js

---
+++ unit1.js
@@ -0,0 +1,13 @@
+const unitData = [
+    { word: "淘气", pinyin: "táo qì", meaning: "顽皮；爱捣蛋。", meaning_en: "naughty; mischievous" },
+    { word: "惹", pinyin: "rě", meaning: "招引；引起（不快或麻烦）。", meaning_en: "to provoke; to attract (troubles)" },
+    { word: "两肋插刀", pinyin: "liǎng lèi chā dāo", meaning: "形容为朋友亲情，不惜牺牲自己。", meaning_en: "sacrifice oneself for friends" },
+    { word: "直率", pinyin: "zhí shuài", meaning: "坦率；直爽。", meaning_en: "candid; frank" },
+    { word: "口头禅", pinyin: "kǒu tóu chán", meaning: "经常挂在嘴边的习惯语。", meaning_en: "catchphrase; mantra" },
+    { word: "人缘", pinyin: "rén yuán", meaning: "指与周围人的关系。", meaning_en: "relations with other people" },
+    { word: "性格", pinyin: "xìng gé", meaning: "人在态度和行为上表现出来的心理特征。", meaning_en: "nature; temperament; character" },
+    { word: "得罪", pinyin: "dé zuì", meaning: "触犯；使不愉快。", meaning_en: "to commit an offense" },
+    { word: "别扭", pinyin: "biè niu", meaning: "不顺心；拘束；不自然。", meaning_en: "awkward; uncomfortable" },
+    { word: "争辩", pinyin: "zhēng biàn", meaning: "争论辩驳。", meaning_en: "a dispute; to wrangle" },
+    { word: "冤枉", pinyin: "yuān wang", meaning: "使无罪者有罪；受到不公平待遇。", meaning_en: "to accuse wrongly; treated unjustly" }
+];
